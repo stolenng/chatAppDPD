f6d96a17dc4b91cd16c026d2e9c2866e873af180
Georgy <stolenng@gmail.com>
2016-03-26 21:46:35 +0300

    ChatAppFix

--- public/js/app.js
+++ public/js/app.js
@@ -1,240 +1,205 @@
-var chatApp = angular.module('ChatApp', ['ui.router','dpd','ngCookies']);
+var chatApp = angular.module('ChatApp', ['ui.router', 'dpd', 'ngCookies']);
 
 chatApp.value('dpdConfig', {
-    collections: ['users','messages'],
+    collections: ['users', 'messages'],
     serverRoot: 'http://localhost:2403/', // optional, defaults to same server
-    socketOptions: { reconnectionDelayMax: 3000 }, // optional socket io additional configuration
+    socketOptions: {reconnectionDelayMax: 3000}, // optional socket io additional configuration
     useSocketIo: true, // optional, defaults to false
     noCache: true // optional, defaults to false (false means that caching is enabled, true means it disabled)
 });
 
 
-chatApp.config( function($stateProvider, $urlRouterProvider) {
+chatApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 
-  $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: 'loginForm.html'
-      })
-      .state('register',{
-          url: '/register',
-          templateUrl: 'registerForm.html'
-      })
-
-
-      .state('chat', {
-          url: '/chat',
-          templateUrl: 'chat.html'
-      });
-
-});
-
-    chatApp.service('messageService',['$q','dpd','$state' ,function ($q,dpd,$state) {
-        this.getAllMesages = function () {
-            var defer = $q.defer();
-            dpd.messages.get().success(function (arr) {
-               defer.resolve(arr);
-            });
-            return defer.promise;
-
-        };
-
-        this.sendMessage = function(msg,userid){
-
-            dpd.messages.post({"text":msg ,"userID":userid})
-                .success(function (result) {
-
-                })
-                .error(function (err) {
-                    if(err) return console.log(err);
-                })
-
-        };
-
-
-    }]);
-
-
-
-chatApp.service('userService',['$rootScope','$q','$window','$timeout','dpd','$cookies','$state','$http'
-                ,function ($rootScope,$q,$window,$timeout,dpd,$cookies,$state,$http){
-
-        var user;
-
-        //Function Get User Name And Password And Logs in
-            this.logIn = function(userName,pass) {
-                        $http.post('/users/login', {
-                            username: userName,
-                            password: pass
-                                }).success(function (userSession) {
-
-                                    $cookies.sid = userSession.id;
-                                    $state.go('chat');
-
-                                }).error(function (err) {
-                                    return err.message;
-                                });
-            }
-        //Function Gets Id Disconnects User
-        this.disconnect= function (id){
-            dpd.users.get('logout').success(function () {
-                dpd.users.put(id,{"connected":false});
-                $state.go('home');
-            });
-        };
-        //Function Returns Promise With User
-        this.checkIfConnected = function () {
-            var defer = $q.defer();
-
-            var user = dpd.users.get('me').success(function (user){
-                if(user)
-                {
-
-                    defer.resolve(user);
-                    $state.go('chat');
-                    return defer.promise;
-
-                }
-                else{
-                    $state.go('home');
-                }
-
-            });
-
-            return user;
-        }
-
-        //Register User - returns true if success
-        this.register = function(name,pass){
-            var defer = $q.defer();
-             dpd.users.post({"username":name ,"password":pass})
-                .success(function (result) {
-                    defer.resolve(true);
-
-                })
-                .error(function (err) {
-                    defer.resolve(false);
-                    if(err) return alert("name " + err.errors.username);
-                });
-            return defer.promise;
-
-        };
- }]);
-
-
-
-
-
-
-chatApp.controller('registerController' , function($scope,userService,$state) {
-    //Go Back To Log In
-    $scope.Back = function () {
-        $state.go('home');
-    }
-    //Submit Register Form And Log In
-    $scope.submit = function(username,password,password2){
-        if(password != password2){
-            return alert("password doesnt match");
-        }
-        userService.register(username,password).then(function (result){
-            if(result == true){
-                userService.logIn(username,password);
-            }
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/loginForm.html'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'templates/registerForm.html'
+        })
+        .state('chat', {
+            url: '/chat',
+            templateUrl: 'templates/chat.html'
         });
 
+}]);
 
+chatApp.service('$messages', ['$q', 'dpd', function ($q, dpd) {
+
+    this.getAllMessages = function () {
+        return dpd.messages.get();
     };
-});
 
-
-
-
-chatApp.controller('chatController' ,['$rootScope','dpd','messageService','userService','$scope','$timeout',
-    function($rootScope,dpd,messageService,userService,$scope,$timeout){
-
-            $scope.user = " ";
-            $scope.messages = [];
-
-        //Checks If User Is Connected
-      userService.checkIfConnected().then(function (result){
-            $scope.user = result.data;
-
-        });
-
-
-            //Reset Messages
-         messageService.getAllMesages().then(function (result) {
-             $scope.messages =result;
-         })
-
-
-            //Check If User Connected
-            $rootScope.$on('$stateChangeStart',
-                function(event, toState, toParams, fromState, fromParams){
-                    //Checks If User Is Connected
-                    userService.checkIfConnected().then(function (result){
-                        $scope.user = result.data;
-                    });
-                })
-
-            //Function To Scroll Chat
-            $scope.scrollChat = function () {
-                var chat = document.getElementById('chat');
-                $("#chat").animate({ scrollTop: chat.scrollHeight }, "slow");
-            };
-            //Scroll Chat On Init
-            $timeout(function () {
-                   $scope.scrollChat();
-                },55);
-
-
-
-
-
-            //Update Messages
-        dpd.on($scope, "msgCreated", function (result) {
-
-            messageService.getAllMesages().then(function (result) {
-                $scope.messages =result;
-            })
-            $scope.scrollChat();
-
-        });
-
-
-
-            //Disconnect User
-            $scope.disconnect = function(){
-
-                userService.disconnect($scope.user.id);
-
-            };
-
-
-
-            //Send Message To Chat
-            $scope.addMessage = function (msg) {
-
-                messageService.sendMessage(msg,$scope.user.id);
-
-            }
+    this.sendMessage = function (msg, userid) {
+        return dpd.messages.post({"text": msg, "userID": userid});
+    };
 
 
 }]);
 
 
-chatApp.controller('usersController',['$scope' ,'userService','$state',function($scope,userService,$state) {
+chatApp.service('$users', ['$q', 'dpd', '$cookies', '$http', function ($q, dpd, $cookies, $http) {
+
+    //Function Get User Name And Password And Logs in
+    this.login = function (Username, Pass) {
+        var defer = $q.defer();
+        $http.post('/users/login', {
+            username: Username,
+            password: Pass
+        }).success(function (UserSession) {
+            $cookies.sid = UserSession.id;
+            defer.resolve();
+        });
+        return defer.promise;
+    };
+
+    //Function Gets Id Disconnects User
+    this.disconnect = function (id) {
+        var defer = $q.defer();
+        dpd.users.get('logout').success(function () {
+            dpd.users.put(id, {"connected": false}).success(function () {
+                defer.resolve();
+            });
+
+        });
+        return defer.promise;
+    };
+
+    //Function Returns Promise With User
+    this.isLogged = function () {
+        return dpd.users.get('me');
+    };
+
+    //Register User - returns true if success
+    this.register = function (name, pass) {
+        return dpd.users.post({"username": name, "password": pass});
+    };
+}]);
+
+
+chatApp.controller('registerController', function ($scope, $users, $state) {
+    //Go Back To Log In
+    $scope.back = function () {
+        $state.go('home');
+    }
+    //Submit Register Form And Log In
+    $scope.submit = function () {
+        if ($scope.password != $scope.password2) {
+            alert("password doesnt match");
+        }
+        else {
+            $users.Register($scope.username, $scope.password).then(function (result) {
+                $users.Login($scope.username, $scope.password).then(function (result) {
+                    $state.go('chat');
+                });
+            }, function (err) {
+                alert(err.message);
+            });
+        }
+
+    };
+});
+
+
+chatApp.controller('chatController', ['dpd', '$state', '$messages', '$users', '$scope', '$timeout',
+    function (dpd, $state, $messages, $users, $scope, $timeout) {
+
+        $scope.user = {};
+        $scope.messages = [];
+
+        //Checks If User Is Connected
+        $users.isLogged().then(function (result) {
+            if (!result.data) {
+                $state.go('home');
+            }
+            $scope.user = result.data;
+        }, function (err) {
+            alert("user Not Logged");
+        });
+
+        //Reset Messages
+        $messages.getAllMessages().then(function (result) {
+
+            $scope.messages = result.data;
+        });
+
+        //Function To Scroll Chat
+        $scope.scrollChat = function () {
+            var chat = document.getElementById('chat');
+            $("#chat").animate({scrollTop: chat.scrollHeight}, "slow");
+        };
+
+        //Scroll Chat On Init
+        $timeout(function () {
+            $scope.scrollChat();
+        }, 55);
+
+        //Update Messages
+        dpd.on($scope, "msgCreated", function (result) {
+            $messages.getAllMessages().then(function (result) {
+                $scope.messages = result.data;
+                $scope.scrollChat();
+            })
+        });
+
+        //Disconnect User
+        $scope.disconnect = function () {
+            $users.disconnect($scope.user.id).then(function () {
+                $state.go('home');
+            });
+        };
+
+        //Send Message To Chat
+        $scope.addMessage = function () {
+            $messages.sendMessage($scope.message, $scope.user.id).then(function () {
+                $scope.message = "";
+            });
+        }
+
+
+    }]);
+
+
+chatApp.controller('usersController', ['$scope', '$users', '$state', function ($scope, $users, $state) {
 
     //Go To Register State
     $scope.register = function () {
         $state.go('register');
     }
     //Log In User
-    $scope.logIn= function(userName,pass){
-        userService.logIn(userName,pass);
+    $scope.login = function () {
+        $users.login($scope.username, $scope.password).then(function (result) {
+            $state.go('chat');
+        });
     }
 
+}]);
+
+chatApp.run(['$rootScope', '$users', function ($rootScope, $users) {
+
+    $rootScope.$on('$stateChangeStart',
+        function (event, toState, toParams, fromState, fromParams) {
+            if (toState.name == 'chat') {
+                $users.isLogged().then(function (result) {
+                    if (!result.data) {
+                        $state.go('home');
+                    }
+                    else {
+                        if (result.data.connected == false) {
+                            $state.go('home');
+                        }
+                        ;
+                    }
+                }, function (err) {
+                    alert("user Not Logged");
+                });
+            }
+
+        });
 
 }]);
